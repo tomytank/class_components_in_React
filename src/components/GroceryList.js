c371a81690fb9841fc232ebc57e7de02b094efa4
Thomas Smith (Tom) <tomytank@users.noreply.github.com>
2020-06-08 22:17:38 +0200

    DeleteItem & EditItem stubs
    Adding deleted items and edit item functions as props in index.js file.

--- src/components/GroceryList.js
+++ src/components/GroceryList.js
@@ -4,11 +4,17 @@ import Item from "./Item";
 //functional component
 const GroceryList = props => {
   //const sortedList = props.groceries.sort((a, b) => a.purchased -b.purchased);
-
+  console.log(props);
   return (
     <div className="shopping-list">
       {props.groceries.map(item => (
-        <Item key={item.id} item={item} toggleItem={props.toggleItem} />
+        <Item
+          key={item.id}
+          item={item}
+          toggleItem={props.toggleItem}
+          clearItem={props.clearItem}
+          editItem={props.editItem}
+        />
       ))}
       <button className="clear-btn" onClick={props.clearPurchased}>
         Clear purchased
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,17 +1,27 @@
 import React from "react";
 //functional component
+const edit = "Edit";
+const clear = "Clear";
 const Item = props => {
   return (
-    <div
-      onClick={() => {
-        props.toggleItem(props.item.id);
-      }}
-      //=> {
-      //console.log("I'm clicked");
-      //  }}
-      className={`item${props.item.purchased ? " purchased" : ""}`}
-    >
-      <p>{props.item.name}</p>
+    <div className={`item${props.item.purchased ? " purchased" : ""}`}>
+      <p
+        onClick={() => {
+          props.toggleItem(props.item.id);
+        }}
+      >
+        {props.item.name}
+      </p>
+      <span onClick={props.editItem} style={{ float: "right", margin: "10px" }}>
+        {clear}&nbsp;
+      </span>
+      <span
+        onClick={props.clearItem}
+        style={{ float: "right", margin: "10px" }}
+      >
+        {" "}
+        {edit}&nbsp;
+      </span>
     </div>
   );
 };
